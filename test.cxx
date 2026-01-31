9bcc557c329c0e27df2e91c7eda6c6b7dacd371c
ARTEM/#_#/ <artem.shid657qq@gmail.com>
2026-01-31 16:52:12 +0300

    Update test.cxx

--- test.cxx
+++ test.cxx
@@ -8,28 +8,61 @@ using namespace std;
 int main(int argc, char *argv[]){
 	//vec3<float>* v=new vec3<float>[10];
 	scene scn;
+	scn.add_obj("objects/Icosahedron.obj");
 	scn.add_obj("objects/test.obj");
-	scn.add_obj("objects/test.obj");
+	
+	scn.set_obj(0,{0,0,0},{0,0,0});
+	scn.set_obj(1,{0,0,3},{0,2,0});
+	scn.set_cam({0,0,-1},{0,0,0});
 	
 	
+	vec3<float> rot={0,0,0},pos={0,0,0};
+	bool flag(0),flag2(0);
 	
-	scn.objects.get(0)->data.pos.z=2;
-	scn.objects.get(1)->data.pos.z=1;
-	
-	screen scr=screen(100,50);
-	scr.fill(' ');
-	scr.draw_scene(&scn,1);
-	cout<<scr.text;
-	cout<<sizeof(scr);
-	
+	while(true)
+	{
+		cout<<"\033[2J";
+		cout<<"\033[?25l";
+		
+		if(_kbhit()){			
+			char ch=_getch();
+			if(flag2)
+			{
+				if(ch=='x'){pos.x+=0.1;}
+				if(ch=='y'){pos.y+=0.1;}
+				if(ch=='z'){pos.z+=0.1;}
+			}else{
+				if(ch=='x'){pos.x-=0.1;}
+				if(ch=='y'){pos.y-=0.1;}
+				if(ch=='z'){pos.z-=0.1;}
+			}
+			if(flag){
+				if(ch=='X'){rot.x+=0.1;}
+				if(ch=='Y'){rot.y+=0.1;}
+				if(ch=='Z'){rot.z+=0.1;}
+			}else{
+				if(ch=='X'){rot.x-=0.1;}
+				if(ch=='Y'){rot.y-=0.1;}
+				if(ch=='Z'){rot.z-=0.1;}
+			}
+			if(ch=='R'){flag=(!flag);}
+			if(ch=='r'){flag2=(!flag2);}
+		}
+		scn.set_cam(pos,rot);
+		screen scr=screen(100,50);
+		scr.fill(' ');
+		scr.draw_scene(&scn,1);
+		cout<<scr.text;
+		for(int i=0;i<10000000;i++){}
+	}
+//	cout<<scr.text;
+
 return 0;} 
 
 /*
 k=l/z
 x*k
 y*k
-
-//код в main можно заменить на код внизу
 
 model Obj;
 	Obj.import("objects/Icosahedron.obj");
